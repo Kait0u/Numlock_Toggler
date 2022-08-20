ae7b38236b696926323788e541553be46bd66639
Jakub Jaworski <jjaw.stuff@gmail.com>
2022-08-20 22:24:03 +0200

    The program now does nothing if NUMLOCK's already on

--- toggler.cpp
+++ toggler.cpp
@@ -2,6 +2,10 @@
 
 int main()
 {
-    keybd_event(VK_NUMLOCK, 0, KEYEVENTF_EXTENDEDKEY, 0);
-    keybd_event(VK_NUMLOCK, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+    auto state = GetKeyState(VK_NUMLOCK);
+    if (!state)
+    {
+        keybd_event(VK_NUMLOCK, 0, KEYEVENTF_EXTENDEDKEY, 0);
+        keybd_event(VK_NUMLOCK, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+    }
 }
